d127c3448b1d537212e8909695b27cd6d1e97c19
Calvin Fernandes <calvin.l.fer@gmail.com>
2024-12-04 14:53:28 -0500

    extension method goes in companion object

--- src/main/scala/io/kaizensolutions/tusk/Transaction.scala
+++ src/main/scala/io/kaizensolutions/tusk/Transaction.scala
@@ -8,17 +8,17 @@ opaque type Transaction = VertxTransaction
 object Transaction:
   def from(txn: VertxTransaction): Transaction = txn
 
-extension [F[_]](using A: Async[F])(txn: Transaction)
-  def commit: F[Unit] = fromVertx(txn.commit()).void
+  extension [F[_]](txn: Transaction)(using A: Async[F])
+    def commit: F[Unit] = fromVertx(txn.commit()).void
 
-  def rollback: F[Unit] = fromVertx(txn.rollback()).void
+    def rollback: F[Unit] = fromVertx(txn.rollback()).void
 
-  def completionStatus: F[TransactionCompletionStatus] =
-    fromVertx(txn.completion()).attempt.map:
-      case Right(_) => TransactionCompletionStatus.Committed
-      case Left(_)  => TransactionCompletionStatus.RolledBack
+    def completionStatus: F[TransactionCompletionStatus] =
+      fromVertx(txn.completion()).attempt.map:
+        case Right(_) => TransactionCompletionStatus.Committed
+        case Left(_)  => TransactionCompletionStatus.RolledBack
 
-extension (txn: Transaction) def escapeHatch: VertxTransaction = txn
+  extension (txn: Transaction) def escapeHatch: VertxTransaction = txn
 
 enum TransactionCompletionStatus:
   case Committed, RolledBack
